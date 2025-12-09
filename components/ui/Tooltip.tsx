10a25b1ccfb9c0aa60ed780f80281d943b30994a
kbandison <kbandison@gmail.com>
2025-12-09 01:14:55 -0500

    Fix tooltip z-index to appear above all elements

--- components/ui/Tooltip.tsx
+++ components/ui/Tooltip.tsx
@@ -27,7 +27,7 @@ export const Tooltip: React.FC<TooltipProps> = ({ content, children, delay = 300
   };
 
   return (
-    <div className="relative inline-block min-w-0 overflow-hidden">
+    <div className="relative inline-block min-w-0">
       <div
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
@@ -36,7 +36,7 @@ export const Tooltip: React.FC<TooltipProps> = ({ content, children, delay = 300
         {children}
       </div>
       {show && (
-        <div className="absolute z-50 bottom-full left-1/2 -translate-x-1/2 mb-2 pointer-events-none">
+        <div className="absolute z-[9999] bottom-full left-1/2 -translate-x-1/2 mb-2 pointer-events-none">
           <div className="bg-brand-brown text-brand-tan px-3 py-2 rounded-lg shadow-lg text-xs font-medium whitespace-nowrap">
             {content}
             <div className="absolute top-full left-1/2 -translate-x-1/2 -mt-1">
